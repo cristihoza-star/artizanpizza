2978fb1bc75b3bd02d4472e909c610c6053e72e9
Artizan Pizza <contact@artizanpizza.ro>
2026-02-04 23:07:10 +0200

    Corectat cai imagini si redenumit fisiere header si logo

--- produse.js
+++ produse.js
@@ -1,24 +1,24 @@
 const products = [
     // Pizza
-    { img: 'img/gallery/01.jpg', title: 'Fusce dictum finibus', desc: 'Nam in suscipit nisi, sit amet consectetur metus. Ut sit amet tellus accumsan', price: '$45 / $55' },
-    { img: 'img/gallery/02.jpg', title: 'Aliquam sagittis', desc: 'Nam in suscipit nisi, sit amet consectetur metus. Ut sit amet tellus accumsan', price: '$65 / $70' },
-    { img: 'img/gallery/03.jpg', title: 'Sed varius turpis', desc: 'Nam in suscipit nisi, sit amet consectetur metus. Ut sit amet tellus accumsan', price: '$30.50' },
-    { img: 'img/gallery/04.jpg', title: 'Aliquam sagittis', desc: 'Nam in suscipit nisi, sit amet consectetur metus. Ut sit amet tellus accumsan', price: '$25.50' },
-    { img: 'img/gallery/05.jpg', title: 'Maecenas eget justo', desc: 'Nam in suscipit nisi, sit amet consectetur metus. Ut sit amet tellus accumsan', price: '$80.25' },
-    { img: 'img/gallery/06.jpg', title: 'Quisque et felis eros', desc: 'Nam in suscipit nisi, sit amet consectetur metus. Ut sit amet tellus accumsan', price: '$20 / $40 / $60' },
-    { img: 'img/gallery/07.jpg', title: 'Sed ultricies dui', desc: 'Nam in suscipit nisi, sit amet consectetur metus. Ut sit amet tellus accumsan', price: '$94' },
-    { img: 'img/gallery/08.jpg', title: 'Donec porta consequat', desc: 'Nam in suscipit nisi, sit amet consectetur metus. Ut sit amet tellus accumsan', price: '$15' },
+    { img: '01.jpg', title: 'Fusce dictum finibus', desc: 'Nam in suscipit nisi, sit amet consectetur metus. Ut sit amet tellus accumsan', price: '$45 / $55' },
+    { img: '02.jpg', title: 'Aliquam sagittis', desc: 'Nam in suscipit nisi, sit amet consectetur metus. Ut sit amet tellus accumsan', price: '$65 / $70' },
+    { img: '03.jpg', title: 'Sed varius turpis', desc: 'Nam in suscipit nisi, sit amet consectetur metus. Ut sit amet tellus accumsan', price: '$30.50' },
+    { img: '04.jpg', title: 'Aliquam sagittis', desc: 'Nam in suscipit nisi, sit amet consectetur metus. Ut sit amet tellus accumsan', price: '$25.50' },
+    { img: '05.jpg', title: 'Maecenas eget justo', desc: 'Nam in suscipit nisi, sit amet consectetur metus. Ut sit amet tellus accumsan', price: '$80.25' },
+    { img: '06.jpg', title: 'Quisque et felis eros', desc: 'Nam in suscipit nisi, sit amet consectetur metus. Ut sit amet tellus accumsan', price: '$20 / $40 / $60' },
+    { img: '07.jpg', title: 'Sed ultricies dui', desc: 'Nam in suscipit nisi, sit amet consectetur metus. Ut sit amet tellus accumsan', price: '$94' },
+    { img: '08.jpg', title: 'Donec porta consequat', desc: 'Nam in suscipit nisi, sit amet consectetur metus. Ut sit amet tellus accumsan', price: '$15' },
     // Salad
-    { img: 'img/gallery/04.jpg', title: 'Salad Menu One', desc: 'Proin eu velit egestas, viverra sapien eget, consequat nunc. Vestibulum tristique', price: '$25' },
-    { img: 'img/gallery/03.jpg', title: 'Second Title Salad', desc: 'Proin eu velit egestas, viverra sapien eget, consequat nunc. Vestibulum tristique', price: '$30' },
-    { img: 'img/gallery/05.jpg', title: 'Third Salad Item', desc: 'Proin eu velit egestas, viverra sapien eget, consequat nunc. Vestibulum tristique', price: '$45' },
-    { img: 'img/gallery/01.jpg', title: 'Superior Salad', desc: 'Proin eu velit egestas, viverra sapien eget, consequat nunc. Vestibulum tristique', price: '$50' },
-    { img: 'img/gallery/08.jpg', title: 'Sed ultricies dui', desc: 'Proin eu velit egestas, viverra sapien eget, consequat nunc. Vestibulum tristique', price: '$55' },
+    { img: '04.jpg', title: 'Salad Menu One', desc: 'Proin eu velit egestas, viverra sapien eget, consequat nunc. Vestibulum tristique', price: '$25' },
+    { img: '03.jpg', title: 'Second Title Salad', desc: 'Proin eu velit egestas, viverra sapien eget, consequat nunc. Vestibulum tristique', price: '$30' },
+    { img: '05.jpg', title: 'Third Salad Item', desc: 'Proin eu velit egestas, viverra sapien eget, consequat nunc. Vestibulum tristique', price: '$45' },
+    { img: '01.jpg', title: 'Superior Salad', desc: 'Proin eu velit egestas, viverra sapien eget, consequat nunc. Vestibulum tristique', price: '$50' },
+    { img: '08.jpg', title: 'Sed ultricies dui', desc: 'Proin eu velit egestas, viverra sapien eget, consequat nunc. Vestibulum tristique', price: '$55' },
     // Noodle
-    { img: 'img/gallery/08.jpg', title: 'Noodle One', desc: 'Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', price: '$12.50' },
-    { img: 'img/gallery/07.jpg', title: 'Noodle Second', desc: 'Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', price: '$15.50' },
-    { img: 'img/gallery/06.jpg', title: 'Third Soft Noodle', desc: 'Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', price: '$20.50' },
-    { img: 'img/gallery/05.jpg', title: 'Aliquam sagittis', desc: 'Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', price: '$30.25' },
-    { img: 'img/gallery/04.jpg', title: 'Maecenas eget justo', desc: 'Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', price: '$35.50' },
-    { img: 'img/gallery/03.jpg', title: 'Quisque et felis eros', desc: 'Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', price: '$40.00' }
+    { img: '08.jpg', title: 'Noodle One', desc: 'Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', price: '$12.50' },
+    { img: '07.jpg', title: 'Noodle Second', desc: 'Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', price: '$15.50' },
+    { img: '06.jpg', title: 'Third Soft Noodle', desc: 'Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', price: '$20.50' },
+    { img: '05.jpg', title: 'Aliquam sagittis', desc: 'Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', price: '$30.25' },
+    { img: '04.jpg', title: 'Maecenas eget justo', desc: 'Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', price: '$35.50' },
+    { img: '03.jpg', title: 'Quisque et felis eros', desc: 'Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', price: '$40.00' }
 ];
